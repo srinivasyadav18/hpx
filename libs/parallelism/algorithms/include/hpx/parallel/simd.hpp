240d3914ae396d4b29b77351f89aec6bd410dcd1
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-05-23 19:50:13 +0530

    Adapt simd.hpp

---
+++ libs/parallelism/algorithms/include/hpx/parallel/simd.hpp
@@ -0,0 +1,20 @@
+//  Copyright (c) 2021 Srinivas Yadav
+//  Copyright (c) 2016 Hartmut Kaiser
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#if defined(HPX_HAVE_CXX20_EXPERIMENTAL_SIMD)
+
+#include <hpx/executors/simd/execution_policy.hpp>
+#include <hpx/parallel/simd/iterator_helpers.hpp>
+#include <hpx/parallel/simd/loop.hpp>
+#include <hpx/parallel/simd/transform_loop.hpp>
+#include <hpx/parallel/simd/zip_iterator.hpp>
+
+#endif
