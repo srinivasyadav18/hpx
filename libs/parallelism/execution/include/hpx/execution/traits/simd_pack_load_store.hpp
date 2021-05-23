c285d87abe89018d4cb72ff199dc1614d2dd8636
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-05-23 19:13:06 +0530

    Adapt simd_pack_load_store

---
+++ libs/parallelism/execution/include/hpx/execution/traits/simd_pack_load_store.hpp
@@ -0,0 +1,28 @@
+//  Copyright (c) 2021 Srinivas Yadav
+//  Copyright (c) 2016-2017 Hartmut Kaiser
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#if defined(HPX_HAVE_CXX20_EXPERIMENTAL_SIMD)
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace parallel { namespace traits {
+    ///////////////////////////////////////////////////////////////////////////
+    template <typename V, typename ValueType>
+    struct simd_pack_load;
+
+    template <typename V, typename ValueType>
+    struct simd_pack_store;
+}}}    // namespace hpx::parallel::traits
+
+#if !defined(__CUDACC__)
+#include <hpx/execution/traits/detail/simd/simd_pack_load_store.hpp>
+#endif
+
+#endif
