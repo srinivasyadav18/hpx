b0be6d69342d8e8d5d83c429d484b0ad581317bb
srinivasyadav18 <srinivasyadav227@icloud.com>
2021-05-23 19:10:25 +0530

    Adapt simd_pack_type

---
+++ libs/parallelism/execution/include/hpx/execution/traits/simd_pack_type.hpp
@@ -0,0 +1,36 @@
+//  Copyright (c) 2021 Srinivas Yadav
+//  Copyright (c) 2016-2017 Hartmut Kaiser
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <hpx/config.hpp>
+
+#if defined(HPX_HAVE_CXX20_EXPERIMENTAL_SIMD)
+#include <hpx/datastructures/tuple.hpp>
+
+#include <cstddef>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace hpx { namespace parallel { namespace traits {
+    ///////////////////////////////////////////////////////////////////////////
+    // exposition only
+    template <typename T, std::size_t N = 0, typename Abi = void>
+    struct simd_pack_type;
+
+    // handle tuple<> transformations
+    template <typename... T, std::size_t N, typename Abi>
+    struct simd_pack_type<hpx::tuple<T...>, N, Abi>
+    {
+        typedef hpx::tuple<typename simd_pack_type<T, N, Abi>::type...> type;
+    };
+}}}    // namespace hpx::parallel::traits
+
+#if !defined(__CUDACC__)
+#include <hpx/execution/traits/detail/simd/simd_pack_type.hpp>
+#endif
+
+#endif
